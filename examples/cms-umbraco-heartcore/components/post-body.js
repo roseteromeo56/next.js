4092064c792f27d5f48df8415a86f0656fd513d2
datadog-bits <263423550+datadog-bits@users.noreply.github.com>
2026-07-02 16:12:44 +0000

    Sanitize Umbraco post body HTML

--- examples/cms-umbraco-heartcore/components/post-body.js
+++ examples/cms-umbraco-heartcore/components/post-body.js
@@ -1,10 +1,13 @@
 import postStyles from "./post-styles.module.css";
+import DOMPurify from "isomorphic-dompurify";
 
 export default function PostBody({ content }) {
+  const sanitizedContent = DOMPurify.sanitize(content ?? "");
+
   return (
     <div
       className={`max-w-2xl mx-auto post ${postStyles.post}`}
-      dangerouslySetInnerHTML={{ __html: content }}
+      dangerouslySetInnerHTML={{ __html: sanitizedContent }}
     />
   );
 }
