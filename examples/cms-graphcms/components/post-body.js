1351fe89656d777c23e51828fe14b624c872d7e1
datadog-bits <263423550+datadog-bits@users.noreply.github.com>
2026-07-02 16:10:59 +0000

    sanitize graphcms post body html

--- examples/cms-graphcms/components/post-body.js
+++ examples/cms-graphcms/components/post-body.js
@@ -1,10 +1,13 @@
+import sanitizeHtml from "sanitize-html";
 import postStyles from "./post-styles.module.css";
 
 export default function PostBody({ content }) {
+  const sanitizedContent = sanitizeHtml(content?.html ?? "");
+
   return (
     <div
       className={`max-w-2xl mx-auto post ${postStyles.post}`}
-      dangerouslySetInnerHTML={{ __html: content?.html }}
+      dangerouslySetInnerHTML={{ __html: sanitizedContent }}
     />
   );
 }
