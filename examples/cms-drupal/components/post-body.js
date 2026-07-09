29db41072588350a6efb91da8e051ec79a7aee30
datadog-bits <263423550+datadog-bits@users.noreply.github.com>
2026-07-09 16:18:28 +0000

    Sanitize cms-drupal post body HTML

--- examples/cms-drupal/components/post-body.js
+++ examples/cms-drupal/components/post-body.js
@@ -1,11 +1,14 @@
 import styles from "./post-body.module.css";
+import DOMPurify from "isomorphic-dompurify";
 
 export default function PostBody({ content }) {
+  const sanitizedContent = DOMPurify.sanitize(content ?? "");
+
   return (
     <div className="max-w-2xl mx-auto">
       <div
         className={styles.content}
-        dangerouslySetInnerHTML={{ __html: content }}
+        dangerouslySetInnerHTML={{ __html: sanitizedContent }}
       />
     </div>
   );
