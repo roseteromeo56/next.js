8fb3580c6b862a56a178ef135bcfe84692003628
datadog-bits <263423550+datadog-bits@users.noreply.github.com>
2026-07-09 16:18:51 +0000

    Sanitize Storyblok post body HTML

--- examples/cms-storyblok/components/post-body.js
+++ examples/cms-storyblok/components/post-body.js
@@ -1,11 +1,14 @@
 import markdownStyles from "./markdown-styles.module.css";
+import DOMPurify from "isomorphic-dompurify";
 
 export default function PostBody({ content }) {
+  const sanitizedContent = DOMPurify.sanitize(content || "");
+
   return (
     <div className="max-w-2xl mx-auto">
       <div
         className={markdownStyles["markdown"]}
-        dangerouslySetInnerHTML={{ __html: content }}
+        dangerouslySetInnerHTML={{ __html: sanitizedContent }}
       />
     </div>
   );
