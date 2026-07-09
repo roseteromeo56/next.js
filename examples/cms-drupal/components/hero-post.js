af456a4bb46136dc7c81acabb6aae549f9451977
datadog-bits <263423550+datadog-bits@users.noreply.github.com>
2026-07-09 16:18:05 +0000

    Escape hero post title and excerpt

--- examples/cms-drupal/components/hero-post.js
+++ examples/cms-drupal/components/hero-post.js
@@ -21,21 +21,16 @@ export default function HeroPost({
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
         <div>
           <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
-            <Link
-              href={slug}
-              className="hover:underline"
-              dangerouslySetInnerHTML={{ __html: title }}
-            ></Link>
+            <Link href={slug} className="hover:underline">
+              {title}
+            </Link>
           </h3>
           <div className="mb-4 md:mb-0 text-lg">
             <Date dateString={date} />
           </div>
         </div>
         <div>
-          <div
-            className="text-lg leading-relaxed mb-4"
-            dangerouslySetInnerHTML={{ __html: excerpt }}
-          />
+          <div className="text-lg leading-relaxed mb-4">{excerpt}</div>
           <Avatar author={author} />
         </div>
       </div>
