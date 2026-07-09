f63dcbcdcf5025b119b9d6742b24d3aeb2e52d43
datadog-bits <263423550+datadog-bits@users.noreply.github.com>
2026-07-09 16:18:03 +0000

    Escape Prepr excerpt rendering

--- examples/cms-prepr/components/hero-post.js
+++ examples/cms-prepr/components/hero-post.js
@@ -30,10 +30,7 @@ export default function HeroPost({
           )}
         </div>
         <div>
-          <div
-            className="text-lg leading-relaxed mb-4"
-            dangerouslySetInnerHTML={{ __html: excerpt }}
-          ></div>
+          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
           <Avatar name={author.full_name} picture={author.profile_pic[0].url} />
         </div>
       </div>
